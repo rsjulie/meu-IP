4bdcef39095b9b70d6256dea8a17ae67944fea68
Julie Ribeiro da Silva <julie@crmsolucoes.com.br>
2025-10-06 09:02:18 -0300

    feat: add reactivity based on connection

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,13 +1,22 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 
 class HomeController extends GetxController {
   final info = NetworkInfo();
   final RxString wifiIP = ''.obs;
+  final Connectivity _connectivity = Connectivity();
 
   @override
   void onInit() async {
     super.onInit();
     wifiIP.value = await info.getWifiIP() ?? 'none';
+    _connectivity.onConnectivityChanged.listen(_updateWifiIP);
+  }
+
+  _updateWifiIP(_) {
+    info.getWifiIP().then((value) {
+      wifiIP.value = value ?? 'none';
+    });
   }
 }
